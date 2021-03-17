1bab9b4ab62cb333b19f4368081dff2d8a21d2af
TyurinIvan <3464mustang@gmail.com>
2021-03-17 23:45:54 +0300

    preliminary result

---
+++ practic_4/RSA.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sympy import Matrix
+import rsa
+from fermat import fermat
+
+
+def get_inverse(key, M):
+    return int(np.array(Matrix(*key.shape, key.reshape(-1)).inv_mod(M)))
+
+
+# Test part
+# print(fermat(5, 100))
+
+# tmp = 9
+# key = np.array([[pow(tmp, 65537)]])
+# print(get_inverse(key, 324408067905331294061961362648401604850041094409525393))
+
+# print(pow(3, 618970019642690137449562110, 618970019642690137449562111))
+
+key_len = 512
+(public_key, private_key) = rsa.newkeys(key_len)
+# (public_key, private_key) = (rsa.key.PublicKey(33233, 65537), rsa.key.PrivateKey(33233, 65537, 31877, 199, 167))
+
+message = b'ARTISTICALLY'
+
+encrypted = rsa.encrypt(message, public_key)  # Шифрование
+print(encrypted)
+message = rsa.decrypt(encrypted, private_key)  # Расшифрование
+print(message)
